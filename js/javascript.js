af3f5487926c9025087314ecbc18e970903ef76d
Freddie <lin.pengyu@outlook.com>
2022-08-24 19:49:53 +0800

    新增下拉式選單

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.dropdown1').click(function (e) { 
+        e.preventDefault();
+        $('.dropdown1').toggleClass('active');
+        $('.dropdown1-open').stop().slideToggle();
+        if($('.dropdown2-open').is(':visible')){
+            $('.dropdown2-open').hide();
+            $('.dropdown2').toggleClass('active');
+        }
+    });
+    $('.dropdown2').click(function (e) { 
+        e.preventDefault();
+        $('.dropdown2').toggleClass('active');
+        $('.dropdown2-open').stop().slideToggle();
+        if($('.dropdown1-open').is(':visible')){
+            $('.dropdown1-open').hide();
+            $('.dropdown1').toggleClass('active');
+        }
+    });
+});
